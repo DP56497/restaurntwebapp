d5c0ef5e728139c5c4ac90c06e20fddaae1098cb
DP56497 <dakshpatel3131@gmail.com>
2025-07-06 09:31:01 +0530

    Update ForgotPassword.js

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -11,7 +11,7 @@ function ForgotPassword() {
     setMessage("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/forgot-password", {
+      const res = await fetch("https://restaurntwebappb.onrender.com/api/forgot-password", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, newPassword }),
