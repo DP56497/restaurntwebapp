7e777e02bc22db8f2a2c81f1fca00784ac5a4822
DP56497 <dakshpatel3131@gmail.com>
2025-07-06 09:35:54 +0530

    Update OrderRecords.js

--- src/pages/OrderRecords.js
+++ src/pages/OrderRecords.js
@@ -16,7 +16,7 @@ function OrderRecords() {
     try {
       const restaurantName = localStorage.getItem("restaurantName");
       const res = await axios.get(
-        `http://localhost:5000/api/orders?restaurantName=${restaurantName}`
+        `https://restaurntwebappb.onrender.com/api/orders?restaurantName=${restaurantName}`
       );
       setOrders(res.data);
     } catch (err) {
